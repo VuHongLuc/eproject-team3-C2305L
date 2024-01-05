6c466898dec5b4719b6316cef238c6ab5ea5f632
Ngoc Vinh <vinhngocdang203@gmail.com>
2024-01-05 22:43:34 +0700

    xoa

--- login/loginData.php
+++ login/loginData.php
@@ -4,15 +4,10 @@ include "../db.php";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
 
-    function validate($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+    
 
-    $username = validate($_POST['username']);
-    $password = validate($_POST['password']);
+    $username = ($_POST['username']);
+    $password = ($_POST['password']);
 
     if (empty($username)) {
         header("Location: login.php?error=User Name is required");
