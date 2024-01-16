9ec42862e5b702624a5adfd17de4102c093dad63
suntous <63642231+suntous@users.noreply.github.com>
2024-01-16 21:51:03 +0700

    Update warrantyPolicy.php

--- contactUs/warrantyPolicy.php
+++ contactUs/warrantyPolicy.php
@@ -57,44 +57,66 @@
 
             <h2 class="my-3">Warranty policy of OceanGate</h2>
 
-            <p><a href="../index/index.php" class="custom-link bold-link">OceanGate</a> warrants to the original end user that its products are free from defects in materials and workmanship under the terms and conditions set forth herein. Subject to the conditions and limitations set forth below, OceanGate will, as the case may be, repair or replace any part of the product that proves to be defective due to improper manufacturing techniques or materials. Repaired parts or replacement products will be provided by OceanGate on an exchange basis and will be new or recertified. All recertified products have been tested to ensure that they function the same as new products. If OceanGate is unable to repair or replace the product, OceanGate will provide a refund for the lesser of either of the following: the current value of the product at the time of the warranty claim or the purchase price. You must provide proof of purchase showing the date and place of original purchase as well as the product description and price.</p>
+            <p><a href="../index/index.php" class="custom-link bold-link">OceanGate</a> assures the original end user that its products are free from defects in materials and workmanship under the terms and conditions outlined herein. Subject to the conditions and limitations below, OceanGate will repair or replace any defective product part due to improper manufacturing techniques or materials. Repaired parts or replacement products, provided by OceanGate on an exchange basis, will be either new or recertified, with recertified products thoroughly tested to ensure they function as new.</p>
 
-            <p>This limited warranty does not cover any damage to this product resulting from improper installation, accident, abuse, misuse, act of God, excessive power supply, abnormal mechanical or environmental conditions, or unauthorized dismantling, repair, or modification. This limited warranty does not apply if: (i) the product is not used in accordance with the accompanying instructions, or (ii) the product is not used for its intended purpose.</p>
+            <p>If OceanGate cannot repair or replace the product, a refund will be provided for the lesser of the current product value at the warranty claim time or the purchase price. Proof of purchase showing the date, place of original purchase, product description, and price is required.</p>
 
-            <p>This limited warranty also does not apply to any product, the original identification of which has been altered, defaced, defaced, improperly handled or packaged, that has been sold as is. used or resold in contravention of U.S. export regulations and any other applicable export regulations.</p>
+            <p><strong>This limited warranty does not cover:</strong></p>
 
-            <p>This limited warranty covers only repair, replacement, refund or credit for defective OceanGate products as set forth above. OceanGate is not responsible and does not cover under warranty any damages or losses of any kind arising from loss, damage or corruption of content or data, or any costs related thereto. determine the cause of system problems or remove, service or install OceanGate products. This warranty does not cover third-party software, connected devices, or stored data. Therefore, OceanGate is not responsible for any actual or consequential loss or damage that may be attributed to third party software, connected devices or stored data. In the event of a claim, OceanGate's sole and maximum liability will be to repair and replace the hardware or provide a refund as determined by OceanGate Vietnam.</p>
+            <ol>
+                <li> Damage resulting from improper installation, accident, abuse, misuse, act of God, excessive power supply, abnormal mechanical or environmental conditions, or unauthorized dismantling, repair, or modification.</li>
+
+                <li> Instances where the product is not used in accordance with accompanying instructions or not for its intended purpose.</li>
+
+                <li> Any product with altered or defaced original identification, improper handling or packaging, sold as-is, used, or resold against U.S. export regulations and other applicable export regulations.</li>
+            </ol>
+
+            <p><strong>Exclusions from warranty coverage:</strong></p>
+
+            <ol>
+                <li> Loss, damage, or corruption of content or data and associated costs.</li>
+
+                <li> Determining the cause of system problems or removal, service, or installation of OceanGate products.</li>
+
+                <li> Third-party software, connected devices, or stored data, for which OceanGate is not responsible for actual or consequential loss or damage.</li>
+            </ol>
+
+            <p>In case of a claim, OceanGate's sole and maximum liability will be to repair and replace the hardware or provide a refund, as determined by OceanGate Vietnam.</p>
 
             <h2 class="my-3">Lifetime product warranty</h2>
 
             <ul>
                 <li><strong>Lifetime product warranty:</strong> The following OceanGate products are covered by a lifetime product warranty:</li>
-                <ul>
-                    <li>Memory modules include ValueRAM®, HyperX®, Server Premier, Retail Memory, and OceanGate system-specific memory; Flash memory cards (e.g., Secure Digital, Secure Digital HC, and Change Flash).</li>
-                </ul>
+                
+                <ul><li>Memory modules include ValueRAM®, HyperX®, Server Premier, Retail Memory, and OceanGate system-specific memory.</li></ul>
+
+                
+                <ul><li>Flash memory cards (e.g., Secure Digital, Secure Digital HC, and Change Flash).</li></ul>
             </ul>
 
             <ul>
                 <li><strong>5-year warranty:</strong> The following OceanGate products are covered by this warranty for five years from the date of the original retail purchase:</li>
-                <ul>
-                    <li>DataTraveler® USB Drives (except DataTraveler® Workspace), Internal Design Client DRAM (“CBD”), IronKey® USB Drives, and SSDNow® KC100 (solid state drive) and industrial temperature microSD cards.</li>
-                </ul>
+                
+                <ul><li>DataTraveler® USB Drives (except DataTraveler® Workspace).</li></ul>
+                <ul><li>Internal Design Client DRAM (“CBD”).</li></ul>
+                <ul><li>IronKey® USB Drives.</li></ul>
+                <ul><li>SSDNow® KC100 (solid-state drive).</li></ul>
+                <ul><li>Industrial temperature microSD cards.</li></ul>
+      
             </ul>
 
             <ul>
                 <li><strong>Conditional 5-year SSD warranty:</strong> The following OceanGate products are covered by this warranty, whichever of the following events occurs first:</li>
                 <ul>
-                    <li>Five (5) years from the date of purchase by the end user customer;</li>
+                    <li>Five (5) years from the date of purchase by the end-user customer.</li>
                     <li>When the usage of a SATA SSD as measured by OceanGate's SMART 231 attribute, called “SSD Wear Index,” reaches a normalized value of one (1) as shown by the OceanGate SSD Manager (“KSM”);</li>
                     <li>When the usage of an NVME SSD as indicated by the OceanGate “Usage Percentage” Health attribute reaches or exceeds the normalized value of one hundred (100) as shown by KSM.</li>
                 </ul>
             </ul>
 
-            <p>KSM is specified in the product data sheet and is available on the OceanGate website. For SATA SSDs, new unused products will have a wear rating of one hundred (100), while products that have reached the warranty limit will have a wear rating of one (1). For NVMe SSDs, new unused products will have a Usage Percentage value of 0, while products that have reached the warranty limit will have a Usage Percentage value greater than or equal to one hundred (100).</p>
+            <p>KSM is specified in the product data sheet and is available on the OceanGate website. For SATA SSDs, new unused products start with a wear rating of one hundred (100), while products that have reached the warranty limit will have a wear rating of one (1). For NVMe SSDs, new unused products start with a Usage Percentage value of 0, while products that have reached the warranty limit will have a Usage Percentage value greater than or equal to one hundred (100).</p>
 
-            <p>Please refer to the following SSD Warranty table for product-specific warranty information:</p>
-
-            <h2 class="my-3">Free Technical Support</h2>
+            <h2 class="my-4">Free Technical Support</h2>
 
             <p>If you encounter difficulties during installation or use of OceanGate products, you can contact OceanGate Vietnam's Technical Support department.</p>
 
