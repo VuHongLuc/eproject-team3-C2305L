07316ad3571cdc8ba76fe6b732f0fced15af931a
vuhongluc <honglucvu@gmail.com>
2024-01-25 09:29:09 +0700

    add feature check username when sign up

--- login/signUpdata.php
+++ login/signUpdata.php
@@ -20,16 +20,35 @@ if (!empty($_POST['username']) && !empty($_POST['password'])
 
     $dateIntoDb = convertToDate($dob);
 
-    $query = "INSERT INTO `user` (userName, password, dob,  address, phone, email, roleUser)
-    VALUES ('$username', '$hashedPassword',  '$dob', '$address', '$phone', '$email', '$roleUser')";
+    $checkUserName = "SELECT userName FROM `eproject`.`user`";
+    $resultCheckUserName = $conn->query($checkUserName);
+    $flagCheckUserName = true;
+        if ($resultCheckUserName->num_rows > 0) {
+            $rowUserNames = $resultCheckUserName->fetch_assoc();
+            foreach ($rowUserNames as $rowUserName){
+                if ($rowUserName === $username){
+                    $flagCheckUserName = false;
+                }
+            }}
+    if($flagCheckUserName){
+        $query = "INSERT INTO `user` (userName, password, dob,  address, phone, email, roleUser)
+        VALUES ('$username', '$hashedPassword',  '$dob', '$address', '$phone', '$email', '$roleUser')";
+    
+        $result = $conn->query($query);
+    
+        if ($result === TRUE) {
+            echo '<script>alert("Successfully Register")</script>';
+            header('Refresh: 1; URL = login.php');
+        } else {
+            echo "Fail Register";
+        }
 
-    $result = $conn->query($query);
+    }else{
+        echo '<script>alert("Username is already taken!\nPlease use another username!")</script>';
+        header('Refresh: 0; URL = login.php');
 
-    if ($result === TRUE) {
-        echo '<script>alert("Successfully Register")</script>';
-        header('Refresh: 1; URL = login.php');
-    } else {
-        echo "Fail Register";
     }
+
+    
 }
 ?>
